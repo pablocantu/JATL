fb0da3af341c1b084016c92db1efb45de4f807d9
PAndresCantu <pabloandres9521@gmail.com>
2021-04-25 23:32:02 -0500

    Added new input component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class TodoList extends Component {
-    render(): JSX.Element {
-        return (
-            <div>
-                Todo List
+import Input from 'components/common/Input/Input';
+
+const TodoList: React.FC = () => {
+    const [newTodo, setNewTodo] = useState('');
+
+    return (
+        <div className='p-8 bg-c-bg-secondary rounded-md'>
+            <div className='flex flex-row items-end gap-x-4 w-full'>
+                <div className='flex-grow'>
+                    <Input
+                        value={newTodo}
+                        placeholder={newTodo ? 'Awesome! Lets get to it!' : 'What\'s on your mind?'}
+                        onChange={({ target: { value } }) => setNewTodo(value)}
+                    />
+                </div>
+                <button className='bg-c-primary rounded-md py-2 px-4 text-white font-bold font-size text-lg'>+</button>
             </div>
-        )
-    }
+            <div>
+
+            </div>
+        </div>
+    );
 }
 
 export default TodoList;
---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,23 @@
+import React, { InputHTMLAttributes } from 'react';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    value: string | number,
+    placeholder: string
+}
+
+const Input: React.FC<Props> = ({ value, placeholder, ...props }: Props) => {
+    return (
+        <div className='relative'>
+            <div className={`absolute ${value ? '-top-6 text-sm' : 'hidden'}`}>
+                {placeholder}
+            </div>
+            <input
+                className='bg-transparent border-b border-c-primary w-full'
+                placeholder={placeholder}
+                {...props}
+            />
+        </div>
+    );
+}
+
+export default Input;
