995fd9dcb58f6ed617151d1edb902447b35275d6
PAndresCantu <pabloandres9521@gmail.com>
2021-06-02 07:23:15 -0500

    Creates new server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
