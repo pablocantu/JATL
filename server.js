4e8900fad8cf60a1193a41113d26a3f275e021f9
PAndresCantu <pabloandres9521@gmail.com>
2021-06-01 21:56:48 -0500

    Updates server.ts prod configuration and renames the file to server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+// Production configuration
+if (process.env.NODE_ENV === 'production') {
+   // Express will serve up production assets
+   // like our main.js file, or main.css file
+   app.use(express.static('client/build'));
+
+   // Express will serve up index.html file
+   // if it doesn't recognize the route
+   // eslint-disable-next-line @typescript-eslint/no-var-requires
+   const path = require('path')
+   app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
