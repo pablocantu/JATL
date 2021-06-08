967936eb909f1e3429216bbe7d3b9c1e585ce46d
PAndresCantu <pabloandres9521@gmail.com>
2021-06-07 19:43:40 -0500

    Adds z-index values for sidebar and main board

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,10 +6,12 @@ import Board from './Board/Board';
 const Layout: React.FC = () => {
     return (
         <div className='flex flex-col md:flex-row'>
-            <div className='sticky top-0 max-h-min w-full text-right md:max-w-min md:h-screen'>
+            <div className='sticky top-0 max-h-min w-full text-right md:max-w-min md:h-screen z-50'>
                 <Sidebar />
             </div>
-            <Board />
+            <div className='z-40'>
+                <Board />
+            </div>
         </div>
     );
 }
