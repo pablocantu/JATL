aaf11f6c3b5fd3b3a6e44b1a20d817d344e6fe32
PAndresCantu <pabloandres9521@gmail.com>
2021-06-13 19:30:29 -0500

    Refactores mobile board's route strings to use a cosntant variable

--- src/components/Layout/Board/MobileBoard.tsx
+++ src/components/Layout/Board/MobileBoard.tsx
@@ -6,25 +6,29 @@ import TodoList from 'components/Layout/Board/TodoList/TodoList';
 import CompletedTasks from 'components/Layout/Board/CompletedTasks/CompletedTasks';
 import Heroes from 'components/Layout/Board/Heroes/Heroes';
 
+const TASKS_ROUTE = '/tasks';
+const COMPLETED_ROUTE = '/completed';
+const HEROES_ROUTE = '/heroes';
+
 const MobileBoard: React.FC = () => {
     const { pathname } = useLocation();
 
     return (
         <div className='flex flex-col mb-4'>
             <div className='flex flex-wrap'>
-                <Link to='/tasks'>
-                    <NavButton title='Enemies' icon='fas fa-user' detail='(0)' active={pathname === '/tasks'} />
+                <Link to={TASKS_ROUTE}>
+                    <NavButton title='Enemies' icon='fas fa-user' detail='(0)' active={pathname === TASKS_ROUTE} />
                 </Link>
-                <Link to='/completed'>
-                    <NavButton title='Chronicles' icon='fas fa-user' active={pathname === '/completed'} />
+                <Link to={COMPLETED_ROUTE}>
+                    <NavButton title='Chronicles' icon='fas fa-user' active={pathname === COMPLETED_ROUTE} />
                 </Link>
-                <Link to='/heroes'>
-                    <NavButton title='Heroes' icon='fas fa-user' active={pathname === '/heroes'} />
+                <Link to={HEROES_ROUTE}>
+                    <NavButton title='Heroes' icon='fas fa-user' active={pathname === HEROES_ROUTE} />
                 </Link>
             </div>
-            <Route path='/tasks' component={TodoList} />
-            <Route path='/completed' component={CompletedTasks} />
-            <Route path='/heroes' component={Heroes} />
+            <Route path={TASKS_ROUTE} component={TodoList} />
+            <Route path={COMPLETED_ROUTE} component={CompletedTasks} />
+            <Route path={HEROES_ROUTE} component={Heroes} />
         </div>
     );
 }
